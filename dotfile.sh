ee6e0878ff7ae318dd86b86532a1de85dd9c9e47
Hiroshi Saeki <saeki-hiroshi@mki.co.jp>
2018-05-18 21:25:12 +0900

    typo

--- dotfile.sh
+++ dotfile.sh
@@ -2,7 +2,7 @@
 cd ~/
 cp .bashrc bashrc_org
 find dotfiles -maxdepth 1 -name ".[!.]?*" ! -name ".git*" ! -name ".config" -print0 | xargs -0 -I % ln -snf %
-[ -v "$XDG_CONFIG_HOME" ] && conighome=$XDG_CONFIG_HOME || conighome="~/.config"
-mkdir -p $conighome
-cd $conighome
+[ -v "$XDG_CONFIG_HOME" ] && confighome=$XDG_CONFIG_HOME || confighome="~/.config"
+mkdir -p $confighome
+cd $confighome
 find ~/dotfiles/.config -maxdepth 1 -type d -name "[!.]*" -print0 | xargs -0 -I % ln -snf %
