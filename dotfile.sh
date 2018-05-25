9305448f007abc9889fe39d734fe47c756f12ff6
Hiroshi Saeki <saeki-hiroshi@mki.co.jp>
2018-05-25 15:07:40 +0900

    add ln for init.vim

--- dotfile.sh
+++ dotfile.sh
@@ -6,3 +6,4 @@ find dotfiles -maxdepth 1 -name ".[!.]?*" ! -name ".git*" ! -name ".config" -pri
 mkdir -p $confighome
 cd $confighome
 find ~/dotfiles/.config -maxdepth 1 -type d -name "[!.]*" -print0 | xargs -0 -I % ln -snf %
+ln -snf ${HOME}/.vimrc nvim/init.vim
