4f873d2d3f2833ddcb3690c8a579a624d4a8270e
SAEKI Hiroshi <5356955+hirsaeki@users.noreply.github.com>
2020-03-30 01:29:57 +0900

    use 'type' to check command exsistence

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 DOTPATH=~/.dotfiles
 
-if has "git"; then
+if type "git" > /dev/null 2>&1 ; then
     git clone --recursive "$GITHUB_URL" "$DOTPATH"
-elif has curl || has wget; then
+elif type "curl" > /dev/null 2>&1 || type "wget"> /dev/null 2>&1 ; then
     tarball="https://github.com/hirsaeki/dotfiles/archive/master.tar.gz"
 
-    if has "curl"; then
+    if "curl"> /dev/null 2>&1 ; then
         curl -L "$tarball"
-    elif has "wget"; then
+    elif has "wget" > /dev/null 2>&1 ; then
 	wget -O - "$tarball"
     fi | tar "$tarball"
 
