0ecbf077a199946a5956ba407a85d26d9e2a13ee
hirsaki <5356955+hirsaeki@users.noreply.github.com>
2022-12-14 08:19:17 +0000

    Mod: bootstrap.sh typo/Add: gpgagent.conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,10 +15,8 @@ conda update conda -y
 conda config --add channels conda-forge
 conda config --set channel_priority strict
 conda update --all -y
-conda install -y neovim git wget fish trach-cli groff tmux binutils powerline-status jq yq unzip patch fzf deno natsort rust rust-std-x86_64-unknown-linux-gnu powerline-status bash-completion conda-bash-completion qrcode pillow
+conda install -y neovim git wget fish trash-cli groff tmux binutils powerline-status jq yq unzip patch fzf deno natsort rust rust-std-x86_64-unknown-linux-gnu powerline-status bash-completion conda-bash-completion qrcode pillow
 conda clean --all -y
-
-eval "$(~/${CONDA_PATH:=miniconda3}/bin/conda shell.bash hook)"
 
 echo '==> install bitwarden cli'
 echo ''
